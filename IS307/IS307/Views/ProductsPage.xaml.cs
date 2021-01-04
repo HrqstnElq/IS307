c80d61f8e5ef2a8d71ffb700703df8fd517cad4f
shin☘ <huutrimega@gmail.com>
2021-01-04 22:19:23 +0700

    test

--- IS307/IS307/Views/ProductsPage.xaml.cs
+++ IS307/IS307/Views/ProductsPage.xaml.cs
@@ -10,31 +10,13 @@ namespace IS307.Views
     public partial class ProductsPage : ContentPage
     {
 
-        public ProductsPage()
-            : this(new BrandModel
-            {
-                ColorCode = Color.White.ToHex(),
-                PricingCategory = "$$$",
-                HighlightUrl = "https://gigamall.com.vn/data/2019/09/20/11493168_Trung-t%C3%A2m-th%C6%B0%C6%A1ng-m%E1%BA%A1i-GIGAMALL-McDonalds-1.jpg",
-                LogoUrl = "https://gigamall.com.vn/data/2019/09/20/11491513_LOGO-McDonald_s.jpg",
-                LongestWaitingTime = 20,
-                Name = "McDonald's",
-                Rating = 4.5,
-                Remark = "Burgers, America",
-                ShortestWaitingTime = 10,
-            })
-        {
-        }
 
-        public ProductsPage(BrandModel brand)
+        public ProductsPage()
         {
             InitializeComponent();
 
-            BindingContext = new ProductsViewModel(Navigation, brand);
-
+            BindingContext = new ProductsViewModel(Navigation);
             (BindingContext as INotifyPropertyChanged).PropertyChanged += ProductsPage_PropertyChanged;
-
-            lstProducts.Scrolled += LstProducts_Scrolled;
         }
 
         private void ProductsPage_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -59,20 +41,6 @@ namespace IS307.Views
                     (itemContainer.Parent as VisualElement).IsVisible = false;
                 }
             }
-        }
-
-        private void LstProducts_Scrolled(object sender, ItemsViewScrolledEventArgs e)
-        {
-            if (e.VerticalOffset < 152)
-            {
-                container.Margin = new Thickness(0, 152, 0, -152);
-                return;
-            }
-
-            var baseline = e.VerticalOffset - 152;
-            var delta = Math.Max(152 - baseline, -(Device.RuntimePlatform == Device.iOS ? 32 : 24));
-
-            container.Margin = new Thickness(0, delta, 0, Math.Min(-delta, 0));
         }
 
         double originalY = -1;
