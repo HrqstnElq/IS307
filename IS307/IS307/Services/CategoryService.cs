117151c837d6c40ecbda831b79fcfddd0c7e5f80
shin☘ <huutrimega@gmail.com>
2021-01-09 00:14:05 +0700

    update service

---
+++ IS307/IS307/Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using IS307.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IS307.Services
+{
+    public class CategoryService
+    {
+        public List<CategoryModel> GetAllCategory()
+        {
+            var response = Singleton.HttpClient.GetStringAsync("/product/categories").Result;
+            var result = JsonConvert.DeserializeObject<List<CategoryModel>>(response);
+            return result;
+        }
+    }
+}
---
+++ IS307/IS307/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using IS307.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS307.Services
+{
+    public class ProductService
+    {
+        public List<ProductModel> GetProductInCategory(string Category)
+        {
+            var response = Singleton.HttpClient.GetStringAsync($"/product/?category={Category}").Result;
+            var result = JsonConvert.DeserializeObject<List<ProductModel>>(response);
+            return result;
+        }
+
+        public List<ProductModel> GetTopProduct()
+        {
+            var response = Singleton.HttpClient.GetStringAsync("/product").Result;
+            var result = JsonConvert.DeserializeObject<List<ProductModel>>(response);
+            return result.Take(15).ToList();
+        }
+    }
+}
